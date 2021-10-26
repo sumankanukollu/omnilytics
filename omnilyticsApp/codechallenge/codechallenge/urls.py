5fd910ae8496b1bed2ee426f25c6f18d27b76035
suman kanukollu <51794227+sumankanukollu@users.noreply.github.com>
2021-10-26 20:12:52 +0530

    Delete omnilyticsApp/codechallenge/codechallenge directory

--- omnilyticsApp/codechallenge/codechallenge/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.homepage , name='home_page'),
-    path('generate/',views.generate,name='generate_logic'),
-    path('download/', views.download_file,name='download_file'),
-    path('report/',views.report,name='generateddata_analyze'),
-]
